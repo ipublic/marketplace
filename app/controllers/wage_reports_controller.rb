0dc04eea7dcef1e5b4b746fa6b0d9f2d531c0a67
Zachary Landes <zach.g.landes@gmail.com>
2019-01-31 09:47:01 -0500

    adding new report form

--- app/controllers/wage_reports_controller.rb
+++ app/controllers/wage_reports_controller.rb
@@ -1,16 +1,14 @@
-
 require 'pry'
 class WageReportsController <  ApplicationController
   layout 'two_column'
   before_action :authenticate_user!
-  
   def index
     @organization = Parties::OrganizationParty.find(params[:employer_id])
-        
     @all_quarters = @organization.wage_reports.map(&:timespans).flatten.uniq.sort{|a,b| b.begin_on - a.begin_on}
     @current_report =  Wages::WageReport.latest(@organization)
+    @report= Wages::WageReport.new
   end
-  
+
   def show 
     @report = Wages::WageReport.find(params[:id])
     @organization = @report.organization_party
@@ -22,13 +20,14 @@ class WageReportsController <  ApplicationController
   def edit 
     @report = Wages::WageReport.find(params[:id])
     @organization = @report.organization_party
-    # binding.pry
+    # binding.pry binding.
     @entries = @report.wage_entries.sort{|a,b|b.wage.state_total_gross_wages - a.wage.state_total_gross_wages}
     @entry  =  @report.wage_entries.new
     @submission_kinds = Wages::WageEntry::SUBMISSION_KINDS
   end
 
   def create 
+    binding.pry
     @report = Wages::WageReport.new
     @organization = Parties::OrganizationParty.find(params[:employer_id])
   end
