a8a042bb5d6e48257d030f11ca8625daf91e42c7
Dan Thomas <dan.thomas@dc.gov>
2019-02-03 13:12:45 -0500

    WIP on RelationshipRoleFactory - coded but no specs

--- app/models/roles/relationship_role_factory.rb
+++ app/models/roles/relationship_role_factory.rb
@@ -1,7 +1,7 @@
 module Roles
 	class RelationshipRoleFactory
 
-		attr_accessor :party_role
+		attr_accessor :party_role, :related_party_role, :party_relationship
 
 
     def self.call(party, args)
@@ -23,25 +23,30 @@ module Roles
   	# assign party_role to related_party
   	# add new party_role to party_roles
 
-    def initialize(party, args)
-    	@party = party
-    	@party_role = nil
-			@party_role_kind = nil
-			@party_relationship_kind = nil
+    def initialize(party, party_role_kind_key, party_relationship_kind_key, related_party)
+    	@party 												= party
+    	@party_role_kind_key 					= party_role_kind_key
+    	@party_relationship_kind_key 	= party_relationship_kind_key
+    	@related_party 								= related_party
 
-    	@party_role_kind_key 					= args[:party_role_kind_key]
-    	@party_relationship_kind_key 	= args[:party_relationship_kind_key]
-    	@related_party 								= args[:related_party]
+			@party_relationship_kind = nil
+			@party_relationship = nil
+
+			@party_role_kind = nil
+    	@party_role = nil
+
+			@related_party_role_kind = nil
+			@related_party_role = nil
 
     	find_party_role_kind
     	find_party_relationship_kind
+    	verify_party_role_pair
 
     	build_party_role
-    	create_party_relationship
-    end
+    	build_related_party_role
 
-    def build_party_role
-    	@party_role = party.party_roles.build(party_role_kind: @party_role_kind, party_relationship: @party_relationship_kind = nil)
+    	initilize_party_relationship
+    	validate_unique_relationship
     end
 
     def find_party_role_kind
@@ -52,27 +57,48 @@ module Roles
     	@party_relationship_kind = Parties::PartyRelationshipKind.find_by(key: @party_relationship_kind_key)
     end
 
-    def create_party_relationship
-    	@party_role.build_party_relationship(party_relationship_kind: @party_relationship_kind)
+    def verify_party_role_pair
+    	if @party_relationship_kind.party_role_kinds.include?(@party_role_kind)
+    		@related_party_role_kind = (@party_relationship_kind.party_role_kinds - @party_role_kind).first
+    	else
+    		# Error condition, the passed party_role_kind isn't in the relationshiop definition
+    	end
     end
 
-    def is_duplicate_party_role?
+    def initilize_party_relationship
+    	@party_relationship = Parties::PartyRelationship.new(party_relationship_kind: @party_relationship_kind, party_roles: [@party_role, @related_party_role])
     end
 
-    def active_party_roles
+    def validate_unique_relationship
+    	# Error condition, this relationship already exists
+
+    	# compare active relationship with same relationship_kind and party pair 
+
+			# if @party.active_party_roles.detect { |role| @party_relationship_kind.present && 
+			# 																						(role.party_relationship_kind == @party_relationship_kind) &&
+			# 																						(role.party_role_kind == party_role_kind)
+			# 																					}
+			# end
+    end
+
+    def build_party_role
+    	@party_role = party.party_roles.build(party_role_kind: @party_role_kind)
+    end
+
+    def build_related_party_role
+    	@related_party_role = related_party.party_roles.build(party_role_kind: @related_party_role_kind)
     end
 
     def party_role
       @party_role
     end
 
-    protected
+    def related_party_role
+      @related_party_role
+    end
 
-    def assign_application_attributes(args)
-      return nil if args.blank?
-      args.each_pair do |k, v|
-        @benefit_application.send("#{k}=".to_sym, v)
-      end
+    def party_relationship
+      @party_relationship
     end
 
 	end
