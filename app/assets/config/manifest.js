99f9ee232792ebe1fdbcab5a27a1d216a6dea536
SocialChaz <chaz@socialdriver.com>
2019-02-03 08:19:07 -0500

    remove unneeded files

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,11 +1,1 @@
-//= require jquery
-//= require twitter/bootstrap
-
-//= require jquery
-//= require rails-ujs
-//= require turbolinks
-//= require bootstrap
-//= link_tree ../images
 //= require ../javascripts/application.js
-//= link_directory ../stylesheets .css
-//= require_tree .
