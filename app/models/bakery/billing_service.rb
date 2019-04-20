998190545cf7597d2c6ea269c708811fa4faeaa4
Dan Thomas <dan@ideacrew.com>
2019-04-20 18:04:35 -0400

    Fix code indentation

--- app/models/bakery/billing_service.rb
+++ app/models/bakery/billing_service.rb
@@ -1,36 +1,36 @@
 module Bakery
   class BillingService
 
-  def call(**options)
-    new(**options).statement
+    def call(**options)
+      new(**options).statement
+    end
+
+    def initialize(**options)
+      @product = options[:product]
+      @customer = options[:customer]
+
+      @product_price = calculate_price
+      # update_customer_account
+      @statement = generate_statement
+
+      self
+    end
+
+    def calculate_price
+      SellCupcakeJob.perform_later
+      # BillingServiceJob.perform_now
+    end
+
+    def update_customer_account
+    end
+
+    def generate_statement
+      @statement = "statement object TBD"
+    end
+
+    def statement
+      @statement
+    end
+
   end
-
-  def initialize(**options)
-    @product = options[:product]
-    @customer = options[:customer]
-
-    @product_price = calculate_price
-    # update_customer_account
-    @statement = generate_statement
-
-    self
-  end
-
-  def calculate_price
-    SellCupcakeJob.perform_later
-    # BillingServiceJob.perform_now
-  end
-
-  def update_customer_account
-  end
-
-  def generate_statement
-    @statement = "statement object TBD"
-  end
-
-  def statement
-    @statement
-  end
-
-end
 end
