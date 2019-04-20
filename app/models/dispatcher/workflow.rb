f940319a89d4eec6e22ee30d7e25452517965bb0
Dan Thomas <dan@ideacrew.com>
2019-04-20 16:00:56 -0400

    Fixed module declaration

--- app/models/dispatcher/workflow.rb
+++ app/models/dispatcher/workflow.rb
@@ -1,10 +1,10 @@
-class Dispatcher
+module Dispatcher
   class Workflow
     include Mongoid::Document
 
     belongs_to :processable, polymorphic: true
 
-    embeds_many
+    # embeds_many
 
 
     field :tasks, type: Array, default: []
